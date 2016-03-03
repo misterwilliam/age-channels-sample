cf3bbddbc6c38fe22341dcfa126e6bbb2d9b5cd0
misterwilliam <mr.williamchang@gmail.com>
2016-03-02 18:35:02 -0800

    Switched from loading in react through bower to loading it through webpack. (Also enabled babel through webpack)

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-document.write("It works.");
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+  document.getElementById('wrapper')
+);
